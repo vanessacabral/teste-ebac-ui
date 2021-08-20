8bad58eff3abd994662972d606305eec56792532
Vanessa Cabral <vanessa.cabral@mindfulmidia.com>
2021-08-20 12:24:34 -0300

    alterações no teste automatizado

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,8 +2,15 @@
 
 context('Funcionalidade Login', () =>{
 
-    it('Deve fazer login com sucesso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso', () =>{
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -11,7 +18,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
@@ -20,7 +26,6 @@ context('Funcionalidade Login', () =>{
     })
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
